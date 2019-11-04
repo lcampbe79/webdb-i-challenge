19ecd2e4db82464b25e54983082f2aa9a01509fa
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-11-04 14:59:30 -0800

    Able to POST new accounts and GET by id.

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
-const knex = require('../data/dbConfig');
+const db = require('../data/dbConfig');
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  knex
+  db
     .select('*')
     .from('accounts')
     .then(accounts => {
@@ -15,5 +15,30 @@ router.get('/', (req, res) => {
       res.status(500).json({error: "Failed to get accounts from the database"})
     })
 });
+
+router.get('/:id', (req, res) => {
+  db
+    .select('*')
+    .from('accounts')
+    .where('id', '=', req.params.id)
+    .then(accounts => {
+      res.status(200).json(accounts)
+    })
+    .catch(error => {
+      res.status(500).json({error: `Failed to get accounts with the id from the database`})
+    })
+})
+
+router.post('/', (req,res) => {
+  db
+    .insert(req.body, 'id')
+    .into('accounts')
+    .then(ids => {
+      res.status(200).json(ids)
+    })
+    .catch(error => {
+      res.status(500).json({error: `Failed to add account`})
+    })
+})
 
 module.exports = router;
